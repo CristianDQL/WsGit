b616964d9418135e6d2dd832d63719ff649afae0
Cristian.Quiceno <cristian.qprog@gmail.com>
2023-01-30 16:11:20 +0100

    Añado método To String

--- 01_ProyectoGit/src/git/entidad/Persona.java
+++ 01_ProyectoGit/src/git/entidad/Persona.java
@@ -1,5 +1,24 @@
 package git.entidad;
 
 public class Persona {
-
+	private String nombre;
+	private String edad;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getEdad() {
+		return edad;
+	}
+	public void setEdad(String edad) {
+		this.edad = edad;
+	}
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", edad=" + edad + "]";
+	}
+	
+	
 }
