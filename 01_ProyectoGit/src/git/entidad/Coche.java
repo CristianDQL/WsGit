3574e6f49d7413630d2c7e381a0d8d2b1e45a2bb
Cristian.Quiceno <cristian.qprog@gmail.com>
2023-01-31 13:20:37 +0100

    Creada la clase coche

---
+++ 01_ProyectoGit/src/git/entidad/Coche.java
@@ -0,0 +1,31 @@
+package git.entidad;
+
+public class Coche {
+	private String matricula, marca, modelo;
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+
+}
