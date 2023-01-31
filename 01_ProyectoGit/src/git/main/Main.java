7e2700c03d8ca7e3aa5de4a674e9f54a5705aec2
Cristian.Quiceno <cristian.qprog@gmail.com>
2023-01-31 15:59:52 +0100

    creada persona e impresa por pantalla

--- 01_ProyectoGit/src/git/main/Main.java
+++ 01_ProyectoGit/src/git/main/Main.java
@@ -1,11 +1,18 @@
 package git.main;
 
+import git.entidad.Persona;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Bienvenidos a nuestras versiones con Git");
 		
+		Persona p = new Persona();
 		
+		p.setNombre("Steve rogers");
+		p.setEdad("45"); 
+		
+		System.out.println(p);
 	}
 
 }
