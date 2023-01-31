e671ebe39500b5621b8008094f8a22ca7c8fd52d
Cristian.Quiceno <cristian.qprog@gmail.com>
2023-01-31 17:44:19 +0100

    post merge de 3 vías

--- 01_ProyectoGit/src/git/entidad/Direccion.java
+++ 01_ProyectoGit/src/git/entidad/Direccion.java
@@ -1,5 +1,45 @@
 package git.entidad;
 
 public class Direccion {
+	private String tipoVia, nombreVia, codPostal;
+	private Direccion direccion;
 
+	public String getTipoVia() {
+		return tipoVia;
+	}
+
+	public void setTipoVia(String tipoVia) {
+		this.tipoVia = tipoVia;
+	}
+
+	public String getNombreVia() {
+		return nombreVia;
+	}
+
+	public void setNombreVia(String nombreVia) {
+		this.nombreVia = nombreVia;
+	}
+
+	public String getCodPostal() {
+		return codPostal;
+	}
+
+	public void setCodPostal(String codPostal) {
+		this.codPostal = codPostal;
+	}
+
+	public Direccion getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
+
+	@Override
+	public String toString() {
+		return "Direccion [tipoVia=" + tipoVia + ", nombreVia=" + nombreVia + ", codPostal=" + codPostal
+				+ ", direccion=" + direccion + "]";
+	}
+	
 }
